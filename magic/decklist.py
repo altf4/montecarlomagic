4929fca04f6689fdbf1618879109c4a7e84560df
AltF4 <altf4petro@gmail.com>
2019-09-08 16:08:01 -0700

    Huge performance boost  - Deepcopy is apparently super slow

--- magic/decklist.py
+++ magic/decklist.py
@@ -12,17 +12,22 @@ class Decklist():
         """Read decklist from txt file
         """
         with open(filename) as f:
-            self.deck = []
-            lines = f.readlines()
-            lines = [x.strip() for x in lines]
-            id = 1
-            for line in lines:
-                # Parse out the quantity prefix
-                for _ in range(int(line.split(" ", 1)[0])):
-                    cardname = (line.split(" ", 1)[1]).replace(" ", "")
-                    card = getattr(magic.cardlogic, cardname)(id)
-                    id += 1
-                    self.deck.append(card)
+            self.lines = f.readlines()
+            self.lines = [x.strip() for x in self.lines]
+            self._parse_decklist(self.lines)
+
+    def _parse_decklist(self, decklist_strings):
+        """ Parses a list of decklist strings into objects
+        """
+        self.deck = []
+        id = 1
+        for line in decklist_strings:
+            # Parse out the quantity prefix
+            for _ in range(int(line.split(" ", 1)[0])):
+                cardname = (line.split(" ", 1)[1]).replace(" ", "")
+                card = getattr(magic.cardlogic, cardname)(id)
+                id += 1
+                self.deck.append(card)
 
     def __len__(self):
         return len(self.deck)
@@ -30,4 +35,5 @@ class Decklist():
     def get_library(self):
         """Get a sample shuffled library from the decklist
         """
-        return copy.deepcopy(self.deck)
+        self._parse_decklist(self.lines)
+        return self.deck
