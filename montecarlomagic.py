bdfb3974c26b76e1405654c17df87c58832af227
AltF4 <altf4petro@gmail.com>
2019-09-15 20:20:52 -0700

    Better output reporting

--- montecarlomagic.py
+++ montecarlomagic.py
@@ -3,6 +3,7 @@ import argparse
 import progressbar
 import statistics
 import itertools
+from numpy import prod
 
 from magic.card import Card
 from magic.boardstate import Boardstate
@@ -11,7 +12,7 @@ from magic.decklist import Decklist
 
 parser = argparse.ArgumentParser(description='Automating MtG goldfishing')
 parser.add_argument('--turns', '-t', type=int, default=20, help='Maximum number of turns per game')
-parser.add_argument('--matches', '-m', type=int, default=100, help='Matches to simulate')
+parser.add_argument('--matches', '-m', type=int, default=50, help='Matches to simulate')
 parser.add_argument('--verbose', '-v', action='store_true', help='Verbose output')
 parser.add_argument('--draw', '-d', action='store_true', help='Assume we\'re on the draw')
 parser.add_argument('--life', '-l', type=int, default=20, help='Start opponent\'s life total at value')
@@ -22,6 +23,8 @@ def win_condition(boardstate):
         return True
     else:
         return False
+
+total_games_played = 0
 
 def rollout_hand(decklist, hand_ids, cards_put_back, iterations=1000):
     """ Run iterations of the given hand
@@ -35,6 +38,9 @@ def rollout_hand(decklist, hand_ids, cards_put_back, iterations=1000):
     win_stats = []
 
     for i in range(iterations):
+        #TODO Find a better way to track this
+        global total_games_played
+        total_games_played += 1
         # Create the boardstate
         boardstate = Boardstate(decklist,
                                 opening_hand=hand_ids,
@@ -138,10 +144,12 @@ if args.verbose:
 
 # dict of scores by turn, including cascading mulls downward
 final_turn_scores = {}
+mulligan_stats = []
 
 for mull_to in range(1, 8):
     turn_scores = []
 
+    mull_count = 0
     for i in progressbar.progressbar(range(args.matches)):
         starting_hand = decklist.sample_hand(7)
 
@@ -162,8 +170,23 @@ for mull_to in range(1, 8):
             # lower is better here, remember
             if performance > final_turn_scores[mull_to - 1]:
                 performance = final_turn_scores[mull_to - 1]
+                mull_count += 1
         turn_scores.append(performance)
 
     turn_score_mean = statistics.mean(turn_scores)
     final_turn_scores[mull_to] = turn_score_mean
+    mulligan_stats.append(mull_count / args.matches)
+    print("Mulligan'd down: ", mull_count / args.matches * 100, "%")
     print("Mulling to ", mull_to, " kills on turn:", round(turn_score_mean, 6))
+
+print("\n============================")
+print("Results!")
+print("Mull stats:")
+for i in range(len(mulligan_stats)):
+    flip_one = list(mulligan_stats)
+    flip_one[0] = 1 - flip_one[0]
+    print("\tKept on", i+1, ": ", prod(flip_one) * 100, "%")
+    mulligan_stats.pop(0)
+
+print("This list kills on average on turn: ", round(final_turn_scores[7], 6))
+print("Total games played:", total_games_played)
